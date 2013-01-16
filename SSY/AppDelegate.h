4d1afb88f78477fb0882b6f558a16501518841f5
John <john@isnapapps.com>
2013-01-16 11:29:41 -0500

    Initial Commit

---
+++ SSY/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SSY
+//
+//  Created by John Hamilton on 1/16/13.
+//  Copyright (c) 2013 SnapApps LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SSY/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SSY
+//
+//  Created by John Hamilton on 1/16/13.
+//  Copyright (c) 2013 SnapApps LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
